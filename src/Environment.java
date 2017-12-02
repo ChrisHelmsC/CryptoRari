864d711468b40b56bb6fd720d3f27d256f7af992
Christopher Helms <chris.helms.C@gmail.com>
2017-12-02 11:00:22 -0500

    Modified test to grab current price of BTC

--- src/Environment.java
+++ src/Environment.java
@@ -1,5 +1,5 @@
 
 public class Environment {
-	public static final String PASSPHRASE = "PUT_YOUR_PASSPHRASE_HERE";
-	public static final String API_KEY = "PUT_YOUR_API_KEY_HERE";
+	public static final String PASSPHRASE = "k6zkm74egfk";
+	public static final String API_KEY = "74402424c59fb5e5850eb7751cab7c5f";
 }
--- src/InformationHandler.java
+++ src/InformationHandler.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.net.URI;
-
+import java.net.URISyntaxException;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -11,57 +11,63 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import com.coinbase.exchange.api.accounts.*;
 
-//Receives 200 OK response from google
+/*According to the GDAX API, the account based headers are not needed
+ * for just getting public market data on currencies. 
+ * 
+ * Running this test allows us to grab the information for BTC-USD
+ * from the server.
+ */
 
 public class InformationHandler {
 
-	public static void main() {
+	public static void main(String [] args) {
 		//Proof GDAX JAR is working
 		Account myAccount = new Account();
 		myAccount.getBalance();
 		
-		//Get time from the GDAX server
-		String timeStamp = "";
-		try {
-			URI timeUri = new URIBuilder()
-					.setScheme(Constants.HTTP.SCHEME) 
-					.setHost(Constants.GDAX.HOST)
-					.setPath(Constants.GDAX.Paths.TIME)
-					.build();
-			CloseableHttpClient timeClient = HttpClients.createDefault();
-			HttpGet httpTimeGet = new HttpGet();
-		} catch (Exception e) {
+		
+			/*Create URI to get price of BTC. Since this is public market data,
+			 * the GDAX account headers are not required.
+			 * 
+			 * The URI is https://api.gdax.com/products/BTC-USD/ticker
+			 */
+			
+			try {
+				URI btcPriceUri = new URIBuilder()
+						.setScheme(Constants.HTTP.SCHEME)
+						.setHost(Constants.GDAX.HOST)
+						.setPath(Constants.GDAX.MarketPaths.PRODUCTS 
+								+ Constants.GDAX.PRODUCT_IDS.BTC_PRODUCT_ID
+								+ Constants.GDAX.MarketPaths.TICKER)
+						.build();
+				System.out.println("The URI being used is: " + btcPriceUri.toString());
+			
+			//Create request using the URI
+			HttpGet httpGet = new HttpGet(btcPriceUri);
+			
+			//Create client, execute request using client, get response from GDAX
+			CloseableHttpClient httpclient = HttpClients.createDefault();
+			CloseableHttpResponse response1 = httpclient.execute(httpGet);
+			
+			//Get Http Entity which contains body and Bitcoin information
+			HttpEntity entity = response1.getEntity();
+			String body = EntityUtils.toString(entity);
+			
+			//Print out status line containing pay load
+			System.out.println("The server response is: \n" + body);
+			
+			//Release resources
+			EntityUtils.consume(entity);
+			response1.close();
+			
+		} catch (URISyntaxException e) {
+			System.out.println("URI had bad syntax and could not be used.");
 			e.printStackTrace();
-		}
-		
-		
-		CloseableHttpClient httpclient = HttpClients.createDefault();
-		try {
-			//Create GDAX URI
-			URI uri = new URIBuilder()
-					.setScheme(Constants.HTTP.SCHEME)
-					.setHost(Constants.GDAX.HOST)
-					.setPath(Constants.GDAX.Paths.CURRENCIES)
-					.build();
-			
-			//Create request
-			HttpGet httpGet = new HttpGet(uri);
-			
-			//Set headers
-			
-			httpGet.setHeader(Constants.GDAX.Headers.CB_ACCESS_KEY, Environment.API_KEY);
-			httpGet.setHeader(Constants.GDAX.Headers.CB_ACCESS_TIMESTAMP, );
-			httpGet.setHeader(Constants.GDAX.Headers.CS_ACCESS_PASSPHRASE, Environment.PASSPHRASE);
-			
-			CloseableHttpResponse response1;
-			
-			//Execute request
-			response1 = httpclient.execute(httpGet);
-			System.out.println(response1.getStatusLine());
-			HttpEntity entity1 = response1.getEntity();
-			EntityUtils.consume(entity1);
-		response1.close();
-		} catch (Exception e) {
+		} catch (ClientProtocolException e) {
+			System.out.println("Client protocol error");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error closing the HttpResponse");
 			e.printStackTrace();
 		}
 	}
